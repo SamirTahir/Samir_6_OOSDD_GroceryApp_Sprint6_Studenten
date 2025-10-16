760b7b3e24b93226150eed5ddea9acd9502055ef
Samir Tahir <S1212989@student.windesheim.nl>
2025-10-16 15:33:34 +0200

    refactor: update GroceryListItemsView UI and repository with database support

--- Grocery.App/Views/GroceryListItemsView.xaml.cs
+++ Grocery.App/Views/GroceryListItemsView.xaml.cs
@@ -1,12 +1,25 @@
 using Grocery.App.ViewModels;
 
-namespace Grocery.App.Views;
-
-public partial class GroceryListItemsView : ContentPage
+namespace Grocery.App.Views
 {
-	public GroceryListItemsView(GroceryListItemsViewModel viewModel)
-	{
-		InitializeComponent();
-        BindingContext = viewModel;
+    public partial class GroceryListItemsView : ContentPage
+    {
+        public GroceryListItemsView(GroceryListItemsViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            (BindingContext as GroceryListItemsViewModel)?.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            (BindingContext as GroceryListItemsViewModel)?.OnDisappearing();
+        }
     }
 }
